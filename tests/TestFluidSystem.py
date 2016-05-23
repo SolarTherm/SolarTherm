919e62febccdd99b1683cba29c2393eabab331ed
Paul Scott <paul.m.sco@gmail.com>
2016-05-23 11:22:20 +1000

    Added tests in for couple of full systems

---
+++ tests/TestFluidSystem.py
@@ -0,0 +1,31 @@
+#! /bin/env python2
+
+from __future__ import division
+import unittest
+
+from solartherm import simulation
+from solartherm import postproc
+
+from math import pi
+
+class TestScheduler(unittest.TestCase):
+	def setUp(self):
+		fn = '../examples/FluidSystem.mo'
+		sim = simulation.Simulator(fn)
+		sim.compile_model()
+		sim.compile_sim(args=['-s'])
+		sim.simulate(start=0, stop='1y', step='5m')
+		self.res = postproc.SimResult(sim.res_fn)
+		self.perf = self.res.calc_perf()
+
+	def test_sched(self):
+		# Note these are set to the values for what is thought to be a working
+		# version.  They are not validated against anything or independently
+		# calculated.
+		self.assertAlmostEqual(self.perf[0], 329.94, 2) # epy
+		self.assertAlmostEqual(self.perf[1], 111.55, 2) # LCOE
+		self.assertAlmostEqual(self.perf[2], 37.66, 2) # Capacity factor
+		print(self.perf);
+
+if __name__ == '__main__':
+	unittest.main()
