25d19ecc8e43051cd9c08faf2b2716865e3ad42b
John Pye <john.pye@anu.edu.au>
2016-11-09 11:41:35 +1100

    working on issue #57.

--- tests/TestCatromOE.mo
+++ tests/TestCatromOE.mo
@@ -1,14 +1,15 @@
 block TestCatromOE
 	import CN = Modelica.Constants;
 	import SolarTherm.Models.CSP.CRS.HeliostatsField.CatromOE;
+	import Modelica.Utilities.Files.loadResource;
 	CatromOE oeff(
-		file="resources/optics/AliceSprings_N10M24_ext.csv",
+		file=loadResource("resources/optics/AliceSprings_N10M24_ext.csv"),
 		n = 10,
 		m = 24,
 		sym = "E"
 		);
 	SolarTherm.Models.Sources.Weather.WeatherSource wea(
-		file="resources/weather/AUS_NT.Alice.Springs.Airport.943260_RMY.motab"
+		file=loadResource("resources/weather/AUS_NT.Alice.Springs.Airport.943260_RMY.motab")
 		);
 equation
 	connect(wea.wbus, oeff.wbus);
--- tests/TestCatromOE.py
+++ tests/TestCatromOE.py
@@ -5,16 +5,17 @@ from solartherm import simulation, postproc
 
 class TestCatromOE(unittest.TestCase):
 	def setUp(self):
+		print "RUNNING SETUP"
 		fn = 'TestCatromOE.mo'
-		sim = simulation.Simulator(fn)
+		sim = simulation.Simulator(fn,tempdir=None)
+		print "COMPILING MODEL"
 		sim.compile_model()
 		sim.compile_sim(args=['-s'])
 		sim.simulate(start=0, stop='1y', step=300)
 		self.res = postproc.SimResult(sim.model + '_res.mat')
 
 	def test_opt_eff_interp(self):
-		pass
-		#self.assertAlmostEqual(self.res.interpolate('oeff.eff[1]', 0.1), 0)
+		self.assertAlmostEqual(self.res.interpolate('oeff.eff[1]', 0.1), 0)
 
 if __name__ == '__main__':
 	unittest.main()
