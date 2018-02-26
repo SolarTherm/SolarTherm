71a87342c5e2b1c1b3db6da6ff24b19c52ace381
ashirazi7 <ali.shirazee@gmail.com>
2018-02-26 15:39:04 +1100

    Test cases added for the Reference systems

--- tests/TestReference1Fluid.py
+++ tests/TestReference1Fluid.py
@@ -14,15 +14,17 @@ class TestReference1Fluid(unittest.TestCase):
 		sim = simulation.Simulator(fn)
 		sim.compile_model()
 		sim.compile_sim(args=['-s'])
-		sim.simulate(start=0, stop='1y', step='5m',solver='dassl')
+		sim.simulate(start=0, stop='1y', step='5m',solver='dassl', nls='newton')
 		self.res = postproc.SimResultElec(sim.res_fn)
-
-		# TODO need to restore calc_perf functionality
-		#self.perf = self.res.calc_perf()
+		self.perf = self.res.calc_perf()
 
 	def test_reference1_fluid(self):
-		# TODO need to add specific tests here
-		pass
-
+		# Note these are set to the values for what is thought to be a working
+		# version.  They are not validated against anything or independently
+		# calculated.
+		self.assertAlmostEqual(self.perf[0], 429982.95, 2) # epy
+		self.assertAlmostEqual(self.perf[1], 147.92, 2) # LCOE
+		self.assertAlmostEqual(self.perf[2], 49.08, 2) # Capacity factor
+		print(self.perf);
 if __name__ == '__main__':
 	unittest.main()
