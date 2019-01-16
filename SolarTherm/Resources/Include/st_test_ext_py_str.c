7484611476377a0e4bd84701195f37a693c5cfdf
Ali Shirazi <ali.shirazee@gmail.com>
2019-01-16 15:10:35 +1100

    A string-passing extenral Python function added

---
+++ SolarTherm/Resources/Include/st_test_ext_py_str.c
@@ -0,0 +1,17 @@
+#ifndef ST_TEST_EXT_PY_STR
+#define ST_TEST_EXT_PY_STR
+
+#include <python2.7/Python.h>
+
+int TestExternalPy_string(const char *code);
+
+
+int TestExternalPy_string(const char *code)
+{
+  Py_Initialize();
+  PyRun_SimpleString(code);
+  Py_Finalize();
+  return 15;
+}
+
+#endif
