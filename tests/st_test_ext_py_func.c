bb2ade1404eeb3a0f83301b960d5e25cdd336ea8
Ali Shirazi <ali.shirazee@gmail.com>
2019-01-23 14:04:54 +1100

    An external Python function example added

---
+++ tests/st_test_ext_py_func.c
@@ -0,0 +1,82 @@
+#ifndef ST_TEST_EXT_PY_FUNC
+#define ST_TEST_EXT_PY_FUNC
+
+#include <python2.7/Python.h>
+#include <stdio.h>
+
+double TestExternalPy_func(int argc, char *argv[]);
+
+double TestExternalPy_func(int argc, char *argv[])
+{
+    double output;
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+    if (argc < 3) {
+        fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
+        return 1;
+    }
+
+    Py_Initialize(); /*  Initialize Interpreter  */
+
+    PySys_SetPath(argv[0]);  // path to the module (function file) to import
+
+    pName = PyString_FromString(argv[1]);
+    /* Error checking of pName left out */
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != NULL) {
+        pFunc = PyObject_GetAttrString(pModule, argv[2]);
+        /* pFunc is a new reference */
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(argc - 3);
+            for (i = 0; i < argc - 3; ++i) {
+                //printf("aaa %f\n", atof(argv[i + 3]));
+                //pValue = PyInt_FromLong(atoi(argv[i + 3]));
+                pValue = PyFloat_FromDouble(atof(argv[i + 3]));
+                if (!pValue) {
+                    Py_DECREF(pArgs);
+                    Py_DECREF(pModule);
+                    fprintf(stderr, "Cannot convert argument\n");
+                    return 1;
+                }
+                /* pValue reference stolen here: */
+                PyTuple_SetItem(pArgs, i, pValue);
+            }
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            output = PyFloat_AsDouble(pValue);
+            Py_DECREF(pArgs);
+            if (pValue != NULL) {
+                //printf("Result of call: %ld\n", PyInt_AsLong(pValue));
+                Py_DECREF(pValue);
+            }
+            else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+                return 1;
+            }
+        }
+        else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
+        return 1;
+    }
+    Py_Finalize();
+    return output;
+}
+
+#endif
