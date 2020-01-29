55e1bb71348df5e643fcb45c4ee775131b94c7f2
u6780682 <u6780682@anu.edu.au>
2020-01-30 00:54:40 +1100

    Modifying st_test_ext.py to test the use of CoolProp, but fails

--- SolarTherm/Resources/Include/st_test_ext.py
+++ SolarTherm/Resources/Include/st_test_ext.py
@@ -1,6 +1,9 @@
 #! /bin/env python2
 
+import CoolProp
+from CoolProp.CoolProp import PropsSI
+
 def testExternalPy_multiply(a,b):
 	""" Returns a multiplied by b """
-	c = a * b
+	c = PropsSI("T","P",a,"H",b,"R744")
 	return c
