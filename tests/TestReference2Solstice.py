39d07191d2f3cd0e0513abe73345c648ede40d60
yewang726 <ye.wang@anu.edu.au>
2020-07-17 15:53:01 +1000

    test script of Reference_2_solstice

---
+++ tests/TestReference2Solstice.py
@@ -0,0 +1,34 @@
+#! /bin/env python2
+
+from __future__ import division
+import unittest
+
+from solartherm import simulation
+from solartherm import postproc
+
+from math import pi
+import os
+
+class TestReference2Solstice(unittest.TestCase):
+	def setUp(self):
+		fn = '../examples/Reference_2_solstice.mo'
+		sim = simulation.Simulator(fn)
+		sim.compile_model()
+		sim.compile_sim(args=['-s'])
+		sim.simulate(start=0, stop='1y', step='5m',solver='dassl', nls='newton')
+		self.res = postproc.SimResultElec(sim.res_fn)
+		self.perf = self.res.calc_perf()
+
+	def test_sched(self):
+		# Note these are set to the values for what is thought to be a working
+		# version.  They are not validated against anything or independently
+		# calculated.
+		print self.perf
+		self.assertAlmostEqual(self.perf[0], 358957.53, 2) # epy
+		self.assertAlmostEqual(self.perf[1], 175.42, 2) # LCOE
+		self.assertAlmostEqual(self.perf[2], 40.98, 2) # Capacity factor
+		os.system('rm Reference_2_solstice*')
+    		
+
+if __name__ == '__main__':
+	unittest.main()
