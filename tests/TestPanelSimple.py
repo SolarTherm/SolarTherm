135c1728cb43553ff07f5e9bc11a2db61baeb7bb
arfontalvo <armando.fontalvo@anu.edu.au>
2020-08-05 17:26:11 +1000

    New python test for a panel without pressure drop

---
+++ tests/TestPanelSimple.py
@@ -0,0 +1,22 @@
+#! /bin/env python2
+
+from __future__ import division
+import unittest
+
+from solartherm import simulation
+from solartherm import postproc
+
+class TestScheduler(unittest.TestCase):
+	def setUp(self):
+		fn = 'TestPanelSimple.mo'
+		sim = simulation.Simulator(fn)
+		sim.compile_model()
+		sim.compile_sim(args=['-s'])
+		sim.simulate(start=0, stop=200, step=0.4, solver='dassl')
+
+	def test_sched(self):
+		def getval(n):
+			return self.res.interpolate(n,1)
+
+if __name__ == '__main__':
+	unittest.main()
