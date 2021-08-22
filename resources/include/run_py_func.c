00ad94f80e90aa64688b449447694ee1aca8b3ef
John Pye <john@curioussymbols.com>
2021-08-22 23:16:13 +1000

    removing a few stray files, fix TestSolsticeOELT.py

--- resources/include/run_py_func.c
+++ resources/include/run_py_func.c
@@ -6,88 +6,84 @@ const char* RunSolsticeFunc(const char *ppath, const char *pname
 	, const char *rcv_type, const char *wea_file, int argc
 	, const char *varnames[], const double var[]
 ){
-    // ppath: path of the Python script
-    // pname: name of the Python script
-    // pfunc: name of the Python function
-    // psave: directory of saving the results from mcrt
+	// ppath: path of the Python script
+	// pname: name of the Python script
+	// pfunc: name of the Python function
+	// psave: directory of saving the results from mcrt
 
-    const char *tablefile; //the file of the lookup table
-    PyObject *pName, *pModule, *pFunc;
-    PyObject *pArgs, *pValue, *inputs;
-    int i;
+	const char *tablefile = NULL; //the file of the lookup table
+	PyObject *pName, *pModule, *pFunc;
+	PyObject *pArgs, *pValue, *inputs;
+	int i;
 
-    Py_Initialize(); /*  Initialize Interpreter  */
+	Py_Initialize(); /*  Initialize Interpreter  */
 
-    // add the path of the Python function file to the system path
-    PyObject *sys_path = PySys_GetObject("path");
-    PyList_Append(sys_path, PyUnicode_FromString((char *)ppath));
-  
-    // name of the Python file
-    pName = PyUnicode_FromString(pname);
-    /* Error checking of pName left out */
+	// add the path of the Python function file to the system path
+	PyObject *sys_path = PySys_GetObject("path");
+	PyList_Append(sys_path, PyUnicode_FromString((char *)ppath));
 
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
+	// name of the Python file
+	pName = PyUnicode_FromString(pname);
+	/* Error checking of pName left out */
 
-    if (pModule != NULL) {
-        pFunc = PyObject_GetAttrString(pModule, pfunc);
-        /* pFunc is a new reference */
+	pModule = PyImport_Import(pName);
+	Py_DECREF(pName);
 
-        pArgs =PyTuple_New(1);
+	if(pModule != NULL){
+		pFunc = PyObject_GetAttrString(pModule, pfunc);
+		/* pFunc is a new reference */
 
-        if (pFunc && PyCallable_Check(pFunc)) {
-            inputs = PyDict_New();
-            PyDict_SetItemString(inputs, "casedir", PyUnicode_FromString((char *)psave));
-            PyDict_SetItemString(inputs, "field_type", PyUnicode_FromString((char *)field_type));
-            PyDict_SetItemString(inputs, "rcv_type", PyUnicode_FromString((char *)rcv_type));
-            PyDict_SetItemString(inputs, "wea_file", PyUnicode_FromString((char *)wea_file));
-            for (i = 0; i < argc; ++i) {
+		pArgs =PyTuple_New(1);
 
-                pValue = PyFloat_FromDouble(var[i]);
+		if (pFunc && PyCallable_Check(pFunc)){
+			inputs = PyDict_New();
+			PyDict_SetItemString(inputs, "casedir", PyUnicode_FromString((char *)psave));
+			PyDict_SetItemString(inputs, "field_type", PyUnicode_FromString((char *)field_type));
+			PyDict_SetItemString(inputs, "rcv_type", PyUnicode_FromString((char *)rcv_type));
+			PyDict_SetItemString(inputs, "wea_file", PyUnicode_FromString((char *)wea_file));
+			for (i = 0; i < argc; ++i) {
 
-                if (!pValue) {
-                    Py_DECREF(pArgs);
-                    Py_DECREF(pModule);
-                    fprintf(stderr, "Cannot convert argument\n");
-                }
-                /* pValue reference stolen here: */
-                PyDict_SetItemString(inputs, varnames[i], pValue);
-            }
+				pValue = PyFloat_FromDouble(var[i]);
 
-            PyTuple_SetItem(pArgs, 0, inputs);
+				if(!pValue){
+					Py_DECREF(pArgs);
+					Py_DECREF(pModule);
+					fprintf(stderr, "Cannot convert argument\n");
+				}
+				/* pValue reference stolen here: */
+				PyDict_SetItemString(inputs, varnames[i], pValue);
+			}
 
-            pValue = PyObject_CallObject(pFunc, pArgs);
+			PyTuple_SetItem(pArgs, 0, inputs);
 
-            tablefile=PyBytes_AsString(pValue);
+			pValue = PyObject_CallObject(pFunc, pArgs);
 
-
-            Py_DECREF(pArgs);
-            Py_DECREF(inputs);
-            if (pValue != NULL) {
-                Py_DECREF(pValue);
-            }
-            else {
-                Py_DECREF(pFunc);
-                Py_DECREF(pModule);
-                PyErr_Print();
-                fprintf(stderr,"Call failed\n");
-            }
-        }
-        else {
-            if (PyErr_Occurred())
-                PyErr_Print();
-            fprintf(stderr, "Cannot find function \"%s\"\n", pfunc);
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }
-    else {
-        PyErr_Print();
-        fprintf(stderr, "Failed to load \"%s\"\n", pname);
-    }
+			tablefile=PyBytes_AsString(pValue);
 
 
-    return tablefile;
-    Py_Finalize();
+			Py_DECREF(pArgs);
+			Py_DECREF(inputs);
+			if(pValue != NULL) {
+				Py_DECREF(pValue);
+			}else{
+				Py_DECREF(pFunc);
+				Py_DECREF(pModule);
+				PyErr_Print();
+				fprintf(stderr,"Call failed\n");
+			}
+		}else{
+			if(PyErr_Occurred())PyErr_Print();
+			fprintf(stderr, "Cannot find function \"%s\"\n", pfunc);
+		}
+		Py_XDECREF(pFunc);
+		Py_DECREF(pModule);
+	}else{
+		PyErr_Print();
+		fprintf(stderr, "Failed to load \"%s\"\n", pname);
+	}
+	
+	return tablefile;
+	Py_Finalize();
 }
 
+// vim: ts=4:sw=4:tw=80:noet
