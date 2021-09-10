bd663d92db9301df09a4ce7d9d0d1032ca25f5db
John Pye <john.pye@anu.edu.au>
2021-09-10 20:37:10 +1000

    clearer reporting of test_solstice.

--- tests/test_solstice.py
+++ tests/test_solstice.py
@@ -4,10 +4,16 @@ class TestSolsticePy(unittest.TestCase):
 
 	def test_solstive_version(self):
 		import solsticepy
-		s = solsticepy.find_prog('solstice')
+		try:
+			s = solsticepy.find_prog('solstice')
+			assert s
+		except Exception as e:
+			self.fail("Using solsticepy: could not locate 'solstice'")
 		
 		import subprocess
-		ret = subprocess.run([s,'--version'],check=True,capture_output=True,encoding="utf-8")
+		ret = subprocess.run([s,'--version'],capture_output=True,encoding="utf-8")
+		if ret.returncode != 0:
+			self.fail("Running 'solstice --version' returned an error code")
 		version = ret.stdout
 		assert version.strip() == "Solstice 0.9.0"
 
