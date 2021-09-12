a94b26cf9e8cde2bcad1075d13b3f4bf3c8d621d
John Pye <john.pye@anu.edu.au>
2021-09-12 18:12:00 +1000

    normalising solstice path...

--- tests/test_solstice.py
+++ tests/test_solstice.py
@@ -1,20 +1,22 @@
-import unittest
+import unittest, os
+from pathlib import Path
+import subprocess
 
-class TestSolsticePy(unittest.TestCase):
-
-	def test_solstive_version(self):
+def test_solstice_version():
+	try:
 		import solsticepy
-		try:
-			s = solsticepy.find_prog('solstice')
-			assert s
-		except Exception as e:
-			self.fail("Using solsticepy: could not locate 'solstice'")
-		
-		import subprocess
-		ret = subprocess.run([s,'--version'],capture_output=True,encoding="utf-8")
-		if ret.returncode != 0:
-			self.fail("Running 'solstice --version' returned an error code")
-		version = ret.stdout
-		assert version.strip() == "Solstice 0.9.0"
+		s = Path(solsticepy.find_prog('solstice'))
+		assert s
+		assert s.exists()
+	except Exception as e:
+		self.fail("Using solsticepy: could not locate 'solstice'")
+	
+	call = [s,'--version']
+	print("CALL =",call)
+	ret = subprocess.run(call,capture_output=True,encoding="utf-8")
+	if ret.returncode != 0:
+		self.fail("Running 'solstice --version' returned an error code")
+	version = ret.stdout
+	assert version.strip() == "Solstice 0.9.0"
 
 # vim: ts=4:sw=4:noet:tw=80
