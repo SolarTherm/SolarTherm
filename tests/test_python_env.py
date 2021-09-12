cfbd34accb9f1d9c513a42840438213a24f43e25
John Pye <john.pye@anu.edu.au>
2021-09-12 13:18:03 +1000

    fix run(shell=True...) case on MSYS2

--- tests/test_python_env.py
+++ tests/test_python_env.py
@@ -23,14 +23,20 @@ def test_run_input():
 	assert v == 13
 
 def test_run_shell():
-	import subprocess, sys, shlex
+	import subprocess, sys, shlex, platform
 	n = 6
-	call = "%s -c %s" % (sys.executable,shlex.quote("import mymodule;print(mymodule.add7(%d))"%(n,)))
+	def q(s):
+		"""quote the string differently on Windows vs Linux..."""
+		if platform.system()=="Windows":
+			return "\"%s\""%(s,)
+		import shlex; return shlex.quote(s)
+	call = "%s -c %s" % (sys.executable, q("import mymodule;print(mymodule.add7(%d))"%(n,)))
 	#call = ['echo HELLO']
 	print("CALL=",call)
-	res = subprocess.run(call,capture_output=True,check=True,shell=True)
+	res = subprocess.run(call,capture_output=True,check=True,shell=True,encoding='utf-8')
 	assert res.returncode == 0
 	print("OUTPUT =",res.stdout)
 	v = int(res.stdout.strip())
 	assert v == 13
 
+# vim: ts=4:sw=4:noet:
