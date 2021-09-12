eeb1d09d6ca0bc44d6ac4ebd6e2a0b3d88bf78c4
John Pye <john.pye@anu.edu.au>
2021-09-12 18:55:57 +1000

    clearer checking in test_solstice.py

--- tests/test_solstice.py
+++ tests/test_solstice.py
@@ -1,21 +1,27 @@
 import unittest, os
 from pathlib import Path
-import subprocess
+import subprocess, platform, shutil
 
 def test_solstice_version():
-	try:
-		import solsticepy
-		s = Path(solsticepy.find_prog('solstice'))
-		assert s
-		assert s.exists()
-	except Exception as e:
-		self.fail("Using solsticepy: could not locate 'solstice'")
+	import solsticepy
+	s = solsticepy.find_prog('solstice')
+	assert s
+	if platform.system()=="Windows":
+		assert Path(s).exists()
+	else:
+		# on Linux, we just get 'solstice' which should be in our PATH:
+		p = shutil.which(s)
+		print("PATH=",p)
+		assert p
 	
 	call = [s,'--version']
 	print("CALL =",call)
 	ret = subprocess.run(call,capture_output=True,encoding="utf-8")
+	print("RETURNCODE =",ret.returncode)
+	print("STDERR =",ret.stderr)
+	
 	if ret.returncode != 0:
-		self.fail("Running 'solstice --version' returned an error code")
+		raise RuntimeError("Running 'solstice --version' returned an error code")
 	version = ret.stdout
 	assert version.strip() == "Solstice 0.9.0"
 
