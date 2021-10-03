461a1ad5a7e71253cd16e7cc9cfe4cf2ef354a6b
yewang726 <ye.wang@anu.edu.au>
2021-10-03 23:31:05 +1100

    test_mpi works for both linux and windows

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -3,15 +3,22 @@
 from __future__ import division
 import unittest
 import cleantest
-import os
+import os, platform, subprocess
 import multiprocessing as mp	
 
 class TestMPI(unittest.TestCase):
 	def setUp(self):
+		subprocess.call("mpicc test_mpi.c -o hello_world", shell=True)
 		
-		os.system("mpicc test_mpi.c -o hello_world")
-		os.system("mpirun --use-hwthread-cpus ./hello_world >hello_world.txt")
+		mpirun = os.environ.get('ST_MPIRUN','mpirun')
 		
+		if platform.system()=='Windows':
+			mpirun_call = [mpirun,"./hello_world>hello_world.txt"]
+			subprocess.call(mpirun_call, shell=True)			
+		
+		else:
+			os.system(mpirun+' --use-hwthread-cpus ./hello_world >hello_world.txt')
+
 	def test_mpi(self):
 		with open('hello_world.txt') as f:
 		    lines = f.readlines()
@@ -19,7 +26,10 @@ class TestMPI(unittest.TestCase):
 		n_mpi=len(lines)	
 		n_cpu=mp.cpu_count()			
 		self.assertTrue(n_mpi==n_cpu)
-		os.unlink('hello_world')
+		if platform.system()=='Windows':
+			os.unlink('hello_world.exe')
+		else:
+			os.unlink('hello_world')
 		os.unlink('hello_world.txt')
 		
 if __name__ == '__main__':
