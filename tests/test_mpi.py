20e13e012106a671aa150bb1328915b080e2f602
yewang726 <ye.wang@anu.edu.au>
2021-10-04 21:44:42 +1100

    pre-compile testmpi.c in scons, reframe test_mpi.py to pytest format

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -6,38 +6,37 @@ import cleantest
 import os, platform, subprocess
 import multiprocessing as mp	
 
-class TestMPI(unittest.TestCase):
-	def setUp(self):
-		subprocess.call("mpicc test_mpi.c -o hello_world", shell=True)
+
+
+def test_cmpi():
+	'''
+	Use mpirun to run a pre-compiled C program (testmpi.c)
+	to check if the number of processors are equal to the number of available CPUs
 		
-		mpirun = os.environ.get('ST_MPIRUN','mpirun')
+	'''
+	mpirun = os.environ.get('ST_MPIRUN','mpirun')		
+	
+	if platform.system()=='Windows':
+		mpirun_call = [mpirun,"./testmpi>hello_world.txt"]
+		subprocess.call(mpirun_call, shell=True)				
+	else:
+		os.system(mpirun+' --use-hwthread-cpus ./testmpi >hello_world.txt')
+
+	with open('hello_world.txt') as f:
+	    lines = f.readlines()
+	f.close()
+	i=0
+	for l in lines:
+		if 'Hello world' in l:
+			i+=1
+	n_mpi=i	
+	n_cpu=mp.cpu_count()		
+	print('number of cpus', n_cpu)
+	print('number of pros run by mpi', n_mpi)
+	#print(lines)
 		
-		if platform.system()=='Windows':
-			mpirun_call = [mpirun,"./hello_world>hello_world.txt"]
-			subprocess.call(mpirun_call, shell=True)			
-		
-		else:
-			os.system(mpirun+' --use-hwthread-cpus ./hello_world >hello_world.txt')
-			
-	def test_mpi(self):
-		with open('hello_world.txt') as f:
-		    lines = f.readlines()
-		f.close()
-		i=0
-		for l in lines:
-			if 'Hello world' in l:
-				i+=1
-		n_mpi=i	
-		n_cpu=mp.cpu_count()		
-		print('number of cpus', n_cpu)
-		print('number of pros run by mpi', n_mpi)
-		#print(lines)	
-		self.assertTrue(n_mpi==n_cpu)
-		if platform.system()=='Windows':
-			os.unlink('hello_world.exe')
-		else:
-			os.unlink('hello_world')
-		os.unlink('hello_world.txt')
+	assert n_mpi==n_cpu
+	os.unlink('hello_world.txt')
 		
 if __name__ == '__main__':
 	unittest.main()
