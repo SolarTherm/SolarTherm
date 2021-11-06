196174e310c5639fdba66f5adcd1b25ae424f327
philgun <philipe.gunawan@anu.edu.au>
2021-11-06 13:32:27 +1100

    Test script for on the fly surrogate modelling has been added to test suite. Need to work on testing the surrogate generation using Kriging and ANN So far, NREL PB initialisation has been added to the test suite as well as the assertion value tests/SConscript was modified to incorportate the compilation of testOTFsurrogate.c

---
+++ tests/testOTFsurrogate.c
@@ -0,0 +1,117 @@
+/*
+	Test code for testing on the fly surrogate functionality
+*/
+#include "otf/kriging.h"
+#include "otf/ann.h"
+#include "otf/sscpb.h"
+#include "otf/util.h"
+
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_cblas.h>
+#include <gsl/gsl_blas.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <libgen.h>
+#include <math.h>
+#include <dirent.h>
+//#include <Python.h>
+
+#define MAXLEN 1024
+#define NEW(TYPE) (TYPE*)malloc(sizeof(TYPE))
+#define NEW_ARRAY(TYPE,SIZE) (TYPE*)malloc(sizeof(TYPE)*SIZE)
+
+#define TESTOTF_DEBUG
+#ifdef TESTOTF_DEBUG
+# define MSG(FMT,...) fprintf(stdout,"%s:%d: " FMT "\n",__FILE__,__LINE__,##__VA_ARGS__)
+#else
+# define MSG(...) ((void)0)
+#endif
+
+/*
+	Initialtisation of NREL Power Block. Return 1 if pass, 0 if fail
+*/
+
+char* grab_SolarTherm_path(){
+	char* cwd = NEW_ARRAY(char,MAXLEN);
+	cwd = getcwd(cwd, MAXLEN);
+	
+	char* one_dir_up = NEW_ARRAY(char, MAXLEN);
+	one_dir_up = dirname(cwd);
+		
+	char* SolarTherm_path = NEW_ARRAY(char, MAXLEN);
+	snprintf(SolarTherm_path, MAXLEN, "%s/SolarTherm",one_dir_up);
+	return SolarTherm_path;
+}
+
+int test_initNRELPB(){
+	double P_net = 100000000/0.9; 
+	double T_in_ref_blk = 1073.15;
+	double p_high = 25000000.0;
+	double dT_PHX_cold_approach = 15.0;
+	double eta_isen_mc = 0.89;
+	double eta_isen_rc = 0.89;
+	double eta_isen_t = 0.93;
+	double dT_mc_approach = 6.0;
+	double T_amb_base = 41.0 + 273.15 - dT_mc_approach;
+	char* HTF_name = "CarboHSP";
+	int HTF_choice = 50;
+			
+	char* SolarTherm_path = NEW_ARRAY(char, MAXLEN);
+	SolarTherm_path = grab_SolarTherm_path();
+
+	double T_HTF_cold_des = 823.15;
+
+	double* res = NEW_ARRAY(double,13);
+
+	initNRELPB(
+		P_net, T_in_ref_blk, p_high, 
+		dT_PHX_cold_approach, eta_isen_mc, eta_isen_rc, 
+		eta_isen_t, dT_mc_approach, T_amb_base, HTF_name, HTF_choice, 
+		SolarTherm_path, T_HTF_cold_des, res
+	);
+
+	double dT_PHX_hot_approach = res[12];
+	fprintf(stderr, "dt PHX hot approach at design point = %lf\n", dT_PHX_hot_approach);
+	assert(
+		abs(dT_PHX_hot_approach - 93.831340) < 0.1
+	);
+
+	return 0;
+}
+
+typedef int (TestFunction)(void);
+typedef struct{
+	TestFunction *fn;
+	char *name;
+} NamedFunction;
+#define FN(N) {test_##N, #N}
+const NamedFunction tests[] = {
+	FN(initNRELPB
+	), {NULL,NULL}
+};	
+
+int main(int argc, const char **argv){
+	TestFunction *fn = NULL;
+	if(argc>1){
+		for(const NamedFunction *i = &(tests[0]); i->name != NULL; ++i){
+			if(strcmp(argv[1],i->name)==0)fn = i->fn;
+		}
+	}
+	if(!fn){
+		const char *pname = "testOTFsurrogate";
+		if(argc > 0)pname = argv[0];
+		fprintf(stderr,"%s NAME\n",pname);
+		fprintf(stderr,"Run test routine, return 0 on success. Available NAMEs are:\n");
+		for(const NamedFunction *i = &(tests[0]); i->name != NULL; ++i){
+			fprintf(stderr,"\t%s\n",i->name);
+		}
+		return(1);
+	}
+	MSG("\nTesting OTF surrogate '%s'...\n",argv[1]);
+	int res = (*fn)();
+	MSG("res = %d",res);
+	return res;
+}
---
+++ tests/test_OTF_surrogate.py
@@ -0,0 +1,52 @@
+import pytest
+import subprocess as sp
+import shutil, platform, os
+from pathlib import Path
+import DyMat
+
+import cleantest
+from solartherm import simulation
+from solartherm import postproc
+
+def run_ctest(name):
+	"""
+	This simply runs the C-langage tests in testOTFsurrogate.c.
+	Which is checking the functionalities of the surrogate on the fly.
+	"""
+	exe = './testOTFsurrogate'
+	env = None
+	if platform.system()=="Linux":
+		env = os.environ.copy()
+		env['LD_LIBRARY_PATH']='../src/modelica'
+	proc = sp.run([exe,str(name)],env=env)#stdout=sp.PIPE,stderr=sp.PIPE,env=env)
+	assert proc.returncode == 0
+
+def test_initNRELPB():
+	run_ctest('initNRELPB')
+
+'''
+
+def test_daggett():
+	run_ctest('daggett')
+
+def test_wrap():
+	run_ctest('wrap')
+
+def test_modelica():
+	"""
+	Run the modelica test of STMotab.
+	"""
+	fn = './TestSTMotab.mo'
+	sim = simulation.Simulator(fn)
+	sim.compile_model()
+	sim.compile_sim(args=['-s'])
+	sim.simulate(start=0, stop='1y', step='5m', solver='dassl', nls='homotopy')
+
+	res = DyMat.DyMatFile('TestSTMotab_res.mat')
+	assert res.data("t1")[0] == 31500000.
+	assert res.data("dnival")[0] == 976.
+	assert res.data("dnicol")[0] == 2.
+	
+	cleantest.clean('TestSTMotab')
+'''
+# vim: ts=4:sw=4:noet:tw=80
