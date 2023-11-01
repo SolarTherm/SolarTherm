916909f5bf0898d1af6bd12a7560b7edc79c2e96
John Pye <john.pye@anu.edu.au>
2023-11-01 21:03:35 +1100

    ifdef for windows.h

--- src/modelica/run_py_func.c
+++ src/modelica/run_py_func.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
+#ifdef WIN32
+# include <windows.h>
+#endif
 #include <ModelicaUtilities.h>
 
 #define MSG(FMT, ...) \
